0274c682e935b8dd6ceaf6160a1d33a009ac0cd4
Sandun Karunathilleke <sandunkarunathileke@gmail.com>
2024-04-03 17:46:29 +0530

    Adding react logging page

--- mui/react-app/src/App.jsx
+++ mui/react-app/src/App.jsx
@@ -1,29 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import TemporaryDrawer from './components/navbar';
+// import React, { useState, useEffect } from 'react';
+// import TemporaryDrawer from './components/SignInSide';
+//
+// function App() {
+//   const [data,setData] = useState([{}])
+//   // Fetch data when the component
+//
+//   // useEffect(() => {
+//   //   fetch("http://192.168.1.115:8080/EAPSPage").then(
+//   //     res => res.json()
+//   //   ).then(
+//   //     data => {
+//   //       setData(data)
+//   //       console.log(data)
+//   //     }
+//   //   )
+//   // },[])
+//
+//   return (
+//     <div>
+//         <SignInSide/>
+//     </div>
+//   );
+// }
+//
+// export default App;
+
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './components/SignInSide';
+import HomeBody from './components/homebody'; // Import your Dashboard component
 
 function App() {
-  const [data,setData] = useState([{}])
-  // Fetch data when the component
-
-  // useEffect(() => {
-  //   fetch("http://192.168.1.115:8080/EAPSPage").then(
-  //     res => res.json()
-  //   ).then(
-  //     data => {
-  //       setData(data)
-  //       console.log(data)
-  //     }
-  //   )
-  // },[])
-
   return (
-    <div>
-        <TemporaryDrawer/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/dashboard" element={<HomeBody />} /> {/* Route to DashboardPage component */}
+        <Route path="/" element={<LoginPage />} /> {/* Route to LoginPage component */}
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
 
 
 
